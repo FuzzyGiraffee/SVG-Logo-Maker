56a934f139fd4a25037a7f3373a7a55db3a41bdb
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-04-04 22:23:30 -0500

    created colors array, created verification library, imported methods index.js

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-//TODO: fix reference lnks
 const shapes = require('./lib/shapes')
 const generateSvg = require('./lib/generateSvg')
+const svgColors = require('./lib/svgColors')
+const {lengthVerify, colorVerify, hexVerify} = require('./lib/verification')
 
 const questions = [
     {
-        //TODO: validate that only 3 letters are input, possibly in a ./lib script
+        
         type: 'input',
         name: 'shapeText',
         message: 'Input up to 3 characters that will display inside of your logo',
     },
     {
+        //TODO: create verification of default color input
         type: 'input',
         name: 'shapeTextColor',
-        message: 'Input the color that you want the shape to be. Please limit it to one of these default colors https://johndecember.com/html/spec/colorsvg.html',
+        message: 'Input the color that you want the shape to be. Please limit it to one of these default colors https://johndecember.com/html/spec/colorsvg.html or hex codes',
     }, {
         type: 'list',
         name: 'shape',
         message: 'Choose the shape of your logo',
         choices: ['circle', 'triangle', 'square']
     }, {
+        //TODO: create verification of default color input
         type: 'input',
         name: 'shapeColor',
         message: 'Input the color of your shape. Please limit it to one of these default colors https://johndecember.com/html/spec/colorsvg.html',
     },
 ];
 
-function lengthVerify(response) {
-    let shapeText = response.shapeText
-
-    console.log(shapeText.length);
-    if (shapeText.length <= 3) {
-        return;
-    } else {
-        console.error("Text must be 3 letters long")
-    };
-};
-
 inquirer.prompt(questions)
     .then(function (response) {
         lengthVerify(response)
+        hexVerify(response)
 
+        //TODO: create generateSvg Script to replace 'generateSvg'
         fs.writeFile(`logo.svg`, 'generateSvg', (err) =>
-            err ? console.log(err) : console.log('Success!'))
+            err ? console.log(err) : console.log('Generated logo.svg'))
     });
+
